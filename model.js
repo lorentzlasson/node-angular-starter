27784c6dc485bcc3a3b72129f6a15e972f7b48ca
Lorentz Lasson <lorentz.lasson@se.ibm.com>
2015-10-25 13:17:25 +0100

    unindent code

--- model.js
+++ model.js
@@ -1,84 +1,84 @@
-	var filesystem = require('fs');
-	var models = {};
-	var relationships = {};
+var filesystem = require('fs');
+var models = {};
+var relationships = {};
 
-	var singleton = function singleton() {
-	    var Sequelize = require("sequelize");
-	    var sequelize = null;
-	    var modelsPath = "";
-	    this.setup = function(path, database, username, password, obj) {
-	        modelsPath = path;
+var singleton = function singleton() {
+    var Sequelize = require("sequelize");
+    var sequelize = null;
+    var modelsPath = "";
+    this.setup = function(path, database, username, password, obj) {
+        modelsPath = path;
 
-	        if (arguments.length == 3) {
-	            sequelize = new Sequelize(database, username);
-	        } else if (arguments.length == 4) {
-	            sequelize = new Sequelize(database, username, password);
-	        } else if (arguments.length == 5) {
-	            sequelize = new Sequelize(database, username, password, obj);
-	        }
-	        init();
-	    }
+        if (arguments.length == 3) {
+            sequelize = new Sequelize(database, username);
+        } else if (arguments.length == 4) {
+            sequelize = new Sequelize(database, username, password);
+        } else if (arguments.length == 5) {
+            sequelize = new Sequelize(database, username, password, obj);
+        }
+        init();
+    }
 
-	    this.model = function(name) {
-	        return models[name];
-	    }
+    this.model = function(name) {
+        return models[name];
+    }
 
-	    this.Seq = function() {
-	        return Sequelize;
-	    }
+    this.Seq = function() {
+        return Sequelize;
+    }
 
-	    function init() {
-	        filesystem.readdirSync(modelsPath).forEach(function(name) {
-	            var object = require(modelsPath + "/" + name);
-	            var options = object.options || {}
-	            var modelName = name.replace(/\.js$/i, "");
-	            models[modelName] = sequelize.define(modelName, object.model, options);
-	            if ("relations" in object) {
-	                relationships[modelName] = object.relations;
-	            }
-	        });
+    function init() {
+        filesystem.readdirSync(modelsPath).forEach(function(name) {
+            var object = require(modelsPath + "/" + name);
+            var options = object.options || {}
+            var modelName = name.replace(/\.js$/i, "");
+            models[modelName] = sequelize.define(modelName, object.model, options);
+            if ("relations" in object) {
+                relationships[modelName] = object.relations;
+            }
+        });
 
-	        sequelize.query('SET FOREIGN_KEY_CHECKS = 0')
-	            .then(function() {
-	                return sequelize.sync({
-	                    force: false
-	                });
-	            })
-	            .then(function() {
-	                return sequelize.query('SET FOREIGN_KEY_CHECKS = 1')
-	            })
-	            .then(function() {
-	                console.log('Database synchronised.');
-	            }, function(err) {
-	                console.log(err);
-	            });
+        sequelize.query('SET FOREIGN_KEY_CHECKS = 0')
+            .then(function() {
+                return sequelize.sync({
+                    force: false
+                });
+            })
+            .then(function() {
+                return sequelize.query('SET FOREIGN_KEY_CHECKS = 1')
+            })
+            .then(function() {
+                console.log('Database synchronised.');
+            }, function(err) {
+                console.log(err);
+            });
 
-	        for (var name in relationships) {
-	            var relation = relationships[name];
-	            for (var relName in relation) {
-	                var related = relation[relName];
-	                if (typeof related === "object") {
-	                    models[name][relName](models[related[0]], related[1]);
-	                } else {
-	                    models[name][relName](models[related]);
-	                }
-	            }
-	        }
-	    }
+        for (var name in relationships) {
+            var relation = relationships[name];
+            for (var relName in relation) {
+                var related = relation[relName];
+                if (typeof related === "object") {
+                    models[name][relName](models[related[0]], related[1]);
+                } else {
+                    models[name][relName](models[related]);
+                }
+            }
+        }
+    }
 
-	    if (singleton.caller != singleton.getInstance) {
-	        throw new Error("This object cannot be instantiated");
-	    }
-	}
+    if (singleton.caller != singleton.getInstance) {
+        throw new Error("This object cannot be instantiated");
+    }
+}
 
-	singleton.instance = null;
+singleton.instance = null;
 
-	singleton.getInstance = function() {
-	    if (this.instance === null) {
+singleton.getInstance = function() {
+    if (this.instance === null) {
 
-	        this.instance = new singleton();
-	    }
-	    return this.instance;
-	}
+        this.instance = new singleton();
+    }
+    return this.instance;
+}
 
-	module.exports = singleton.getInstance();
+module.exports = singleton.getInstance();
